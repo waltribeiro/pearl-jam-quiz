6cbcd0f78bc0f62c4e1c9fbfbe70e5f1984255c5
WaltRibeiro <walt@fororchestra.com>
2020-02-11 23:33:38 -0500

    background photo and new questions

--- notes.md
+++
@@ -1,7 +0,0 @@
-### Notes
-
-* 3 links you need to submit
-   * github repo
-   * deployed application
-   * updated portfolio page
-
--- script.js
+++ script.js
@@ -1,113 +1,155 @@
-var choicesPrintVariable = document.getElementById("choicesPrint");
-var questionPrintVariable = document.getElementById("questionPrint");
-var countdownPrintVariable = document.getElementById("countdownPrint");
-var imagePrintVariable = document.getElementById("imagePrint");
-var messagePrintVariable = document.getElementById("messagePrint");
-
-var startButtonVariable = document.getElementById("startButton");
-
-var score = 0;
-var secondsLeft = 60;
-var currentQuestion = 0;
-var correctAnswer;
-
-
-/**
- *
- *
- *
- */
-
-let quizAll = [
+let questions = [
   {
-    question: "What is Eddie's last name?",
-    choices: ["Vedder", "Johnson", "Delaware"],
-    answer: "choice 1"
+    title: "What is Eddie's last name?",
+    choices: ["Vedder", "Johnson", "Delaware", "Rinter"],
+    answer: "Vedder"
   },
   {
-    question: "What is Abbruzzese's first name?",
-    choices: ["Tommy", "Dave", "Benjamin"],
-    answer: "choice 1"
+    title: "What is Abbruzzese's first name?",
+    choices: ["Tommy", "Dave", "Benjamin", "Harold"],
+    answer: "Dave"
   },
   {
-    question: "question 3",
-    choices: ["choice 1", "choice 2", "choice 3", "choice 4",],
-    answer: "choice 1"
+    title: "What was Mike Mccready's band pre-Pearl Jam?",
+    choices: [
+      "Longtown",
+      "AFI",
+      "Soundgarden",
+      "Shadow",
+    ],
+    answer: "Shadow"
   },
   {
-    question: "question 4",
-    choices: ["choice 1", "choice 2", "choice 3", "choice 4",],
-    answer: "choice 1"
+    title: "Who was the drummer on Pearl Jam's 1991 debut album Ten?",
+    choices: ["Mike Garcia", "Dave Krusen", "Ten Townsend", "Marcus Downright",],
+    answer: "Dave Krusen"
   },
   {
-    question: "question 5",
-    choices: ["choice 1", "choice 2", "choice 3", "choice 4",],
-    answer: "choice 1"
+    title: "Where was Pearl Jam's first show played?",
+    choices: ["Off Ramp", "Drive Town", "Score Time", "Hug Bar",],
+    answer: "Off Ramp"
   }
 ];
 
-// Timer Function
+
+
+
+var questionQuiz = document.getElementById("quiz");
+var questionSubmit = document.getElementById("results");
+var score = document.getElementById("score");
+var timer = document.getElementById("timer");
+var startQuiz = document.getElementById("start")
+
+var score = 0;
+var secondsLeft = 75;
+var currentQuestionIndex = 0;
+
+
+
+
+
+
 var timerInterval;
 
 function timer() {
-  if (secondsLeft > 9) {
-    secondsLeft--;
-    countdownPrintVariable.textContent = "0:" + secondsLeft;
-  } else if (secondsLeft > 0) {
-    secondsLeft--;
-    countdownPrintVariable.textContent = "0:0" + secondsLeft;
-  } else {
-    // you lose the game
-    clearInterval(timerInterval);
-    messagePrintVariable.textContent = "you lose!";
-  }
+  if (secondsLeft > 9) {
+    secondsLeft--;
+    countdownPrintVariable.textContent = "0:" + secondsLeft;
+  } else if (secondsLeft > 0) {
+    secondsLeft--;
+    countdownPrintVariable.textContent = "0:0" + secondsLeft;
+  } else {
+    // you lose the game
+    clearInterval(timerInterval);
+    messagePrintVariable.textContent = "you lose!";
+  }
 }
 
-// function questionFunction() {
-// if (x = 0; x < quizAll.length; x++) {
-//   questionPrintVariable.TextContent = quizAll[currentQuestion].question;
-// }
-// }
-
-// Start Game Function
-var startGame = function() {
-  countdownPrintVariable.style = "display: block";
-  timerInterval = setInterval(timer, 1000);
-  // questionFunction()
-
-
-  var questionChosen = quizAll[currentQuestion].question;
-  var choicesChosen = quizAll[currentQuestion].choices;
-  var answerChosen = quizAll[currentQuestion].answer;
-
-
-  questionPrintVariable.textContent = questionChosen;
-  choicesPrintVariable.textContent = choicesChosen;
-
-  document.getElementById("choicesPrint").innerHTML = <button type="button" class="btn btn-info">Choice 3</button>;
-  
-  if (answerChosen == correctAnswer) {
-    score++
-  }
-
-  //anaswer question
-  // if the question has been answered, then currentQuestion++;
-};
-
-// if (choice1Checked === checked) {
-// var whatever
-// }
-
-// function checkA() {
-//   document.getElementById("myCheck").checked = true;
-// }
-
-// function uncheck() {
-//   document.getElementById("myCheck").checked = false;
-// }
-
-
-startButtonVariable.addEventListener("click", startGame);
 
 
 
+
+
+
+
+function goToNextQuestion(whatTheUserClicked) {
+    var correctText = questions[currentQuestionIndex].answer;
+
+    if (whatTheUserClicked == correctText) {
+        console.log("Correct!");
+        score++;
+    }
+    else {
+        console.log("Wrong!");
+    }
+    currentQuestionIndex++;
+    getNewQuestion(currentQuestionIndex);
+}
+
+function answerClickSetup() {
+    var a = document.getElementById("answer1");
+    //console.log(a);
+    var b = document.getElementById("answer2");
+    var c = document.getElementById("answer3");
+    var d = document.getElementById("answer4");
+
+a.addEventListener("click", function () { goToNextQuestion(a.innerText); });
+b.addEventListener("click", function () { goToNextQuestion(b.innerText); });
+c.addEventListener("click", function () { goToNextQuestion(c.innerText); });
+d.addEventListener("click", function () { goToNextQuestion(d.innerText); });
+}
+
+answerClickSetup();
+
+startQuiz.addEventListener("click", function () {
+
+    timerInterval = setInterval(timer, 1000);
+    getNewQuestion(currentQuestionIndex);
+});
+
+var currentQuestion;
+function getNewQuestion(questionIndex) {
+    var question = questions[questionIndex];
+    //console.log(question);
+    currentQuestion = question;
+    var title = question.title;
+    //console.log(title);
+    var questionEl = document.getElementById("question");
+    questionEl.innerText = title;
+
+    var choice1 = question.choices[0];
+    var answerEl1 = document.getElementById("answer1");
+    //console.log(answerEl1);
+    answerEl1.innerText = choice1;
+    
+    var choice2 = question.choices[3];
+    var answerEl2 = document.getElementById("answer2");
+    //console.log(answerEl2);
+    answerEl2.innerText = choice2;
+    //console.log(choice2);
+
+    var choice3 = question.choices[2];
+    var answerEl3 = document.getElementById("answer3");
+    //console.log(answerEl3);
+    answerEl3.innerText = choice3;
+    //console.log(choice3);
+
+    var choice4 = question.choices[0];
+    var answerEl4 = document.getElementById("answer4");
+    //console.log(answerEl4);
+    answerEl4.innerText = choice4;
+    //console.log(choice4);
+
+    document.getElementById("question").innerText = title;
+    document.getElementById("answer1").innerText = choice1;
+    document.getElementById("answer2").innerText = choice2;
+    document.getElementById("answer3").innerText = choice3;
+    document.getElementById("answer4").innerText = choice4;
+
+}
+//submitAnswer.addEventListener("click", quizTime);
+function scoreKeeper(){
+    document.getElementById("score").innerHTML = score++;
+  
+  scoreKeeper();
+}
